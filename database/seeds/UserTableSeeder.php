43317aef7cb4cf034f9743210511675d4ce37c77
Banten IT Solutions <admin@bits.co.id>
2015-06-04 17:53:30 +0700

    BITS.CO.ID

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -13,23 +13,23 @@ class UserTableSeeder extends Seeder
     $user = User::create([
         'name' => 'Administrator',
         'email' => 'admin@bits.co.id',
-        'password' => Hash::make('suck-IT26'),
+        'password' => Hash::make('admin-77'),
     ]);
-    
+
     $plain_user = User::create([
         'name' => 'Retno Intan Purnamasari',
         'email' => 'retno@bits.co.id',
         'password' => Hash::make('retno-77'),
     ]);
-    
-    
+
+
     //Create default roles and permissions
     $admin = new Role();
     $admin->name         = 'admin';
     $admin->display_name = 'Administrator';
     $admin->description  = 'Spesial';
     $admin->save();
-    
+
     $plain = new Role();
     $plain->name         = 'petugas';
     $plain->display_name = 'Petugas';
